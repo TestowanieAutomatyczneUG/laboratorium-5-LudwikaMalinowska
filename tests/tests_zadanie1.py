1d197b0c2514856487cd304a8b2e1c03410e3571
Ludwika Malinowska <malinowska.L@wp.pl>
2020-11-02 16:24:44 +0100

    wrzucam test 2. test_single_letter_identical_strands

--- tests/tests_zadanie1.py
+++ tests/tests_zadanie1.py
@@ -23,7 +23,6 @@ class HammingTest(unittest.TestCase):
     def test_empty_strands(self):
         self.assertEqual(self.temp.distance("", ""), 0)
 
-    @unittest.SkipTest
     def test_single_letter_identical_strands(self):
         self.assertEqual(self.temp.distance("A", "A"), 0)
 
