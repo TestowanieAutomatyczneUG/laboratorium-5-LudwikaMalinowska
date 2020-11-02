50984d8216246d5ad5cbcdf6ed0c6afc5cebb937
Ludwika Malinowska <malinowska.L@wp.pl>
2020-11-02 16:23:26 +0100

    implementacja do testu 1. test_empty_strands

--- tests/tests_zadanie1.py
+++ tests/tests_zadanie1.py
@@ -1,53 +1,63 @@
 import unittest
 
+class Hamming:
+
+    def distance(self, a, b):
+        if a == "" and b == "":
+            return 0
+
+
+
+
 class HammingTest(unittest.TestCase):
 
     # Utility functions
     def setUp(self):
         try:
+            self.temp = Hamming()
             self.assertRaisesRegex
-            # self.temp = Hamming()
+
         except AttributeError:
             self.assertRaisesRegex = self.assertRaisesRegexp
 
     def test_empty_strands(self):
-        self.assertEqual(self.distance("", ""), 0)
+        self.assertEqual(self.temp.distance("", ""), 0)
 
     @unittest.SkipTest
     def test_single_letter_identical_strands(self):
-        self.assertEqual(self.distance("A", "A"), 0)
+        self.assertEqual(self.temp.distance("A", "A"), 0)
 
     @unittest.SkipTest
     def test_single_letter_different_strands(self):
-        self.assertEqual(self.distance("G", "T"), 1)
+        self.assertEqual(self.temp.distance("G", "T"), 1)
 
     @unittest.SkipTest
     def test_long_identical_strands(self):
-        self.assertEqual(self.distance("GGACTGAAATCTG", "GGACTGAAATCTG"), 0)
+        self.assertEqual(self.temp.distance("GGACTGAAATCTG", "GGACTGAAATCTG"), 0)
 
     @unittest.SkipTest
     def test_long_different_strands(self):
-        self.assertEqual(self.distance("GGACGGATTCTG", "AGGACGGATTCT"), 9)
+        self.assertEqual(self.temp.distance("GGACGGATTCTG", "AGGACGGATTCT"), 9)
 
     @unittest.SkipTest
     def test_disallow_first_strand_longer(self):
         with self.assertRaisesWithMessage(ValueError):
-            self.distance("AATG", "AAA")
+            self.temp.distance("AATG", "AAA")
 
     @unittest.SkipTest
     def test_disallow_second_strand_longer(self):
         with self.assertRaisesWithMessage(ValueError):
-            self.distance("ATA", "AGTG")
+            self.temp.distance("ATA", "AGTG")
 
     @unittest.SkipTest
     def test_disallow_left_empty_strand(self):
         with self.assertRaisesWithMessage(ValueError):
-            self.distance("", "G")
+            self.temp.distance("", "G")
 
     @unittest.SkipTest
     def test_disallow_right_empty_strand(self):
         with self.assertRaisesWithMessage(ValueError):
-            self.distance("G", "")
+            self.temp.distance("G", "")
 
     @unittest.SkipTest
     def assertRaisesWithMessage(self, exception):
